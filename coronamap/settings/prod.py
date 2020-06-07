d63a3bdf6c76956beb24ad503f60032cda32717a
Pablo Leano <pleasedontbelong@gmail.com>
2020-06-08 01:03:13 +0200

    Fix whitenoise

--- coronamap/settings/prod.py
+++ coronamap/settings/prod.py
@@ -17,3 +17,33 @@ ALLOWED_HOSTS = ['.%s' % PARENT_HOST]
 SESSION_COOKIE_DOMAIN = ALLOWED_HOSTS[0]
 SESSION_COOKIE_NAME = 'coronamap_sid'
 STATIC_ROOT = ROOT
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'formatters': {
+        'complete': {
+            'format': '[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s'
+                      ':%(lineno)d->%(funcName)s()] %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            "formatter": 'complete',
+        },
+    },
+    'loggers': {
+        'django': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'command': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+    'version': 1,
+}
